e5bf92a8a1b4b9e4a65c3f15ffdac2db96ce82cc
Erbonni <teobon08@gmail.com>
2026-04-22 10:21:42 +0200

    primo commit

---
+++ Dichiarazione_Variabili.h
@@ -0,0 +1,14 @@
+ //
+// Created by Alberto Aquilini on 15/04/26.
+//
+
+#ifndef MAGAZZINO_DICHIARAZIONE_VARIABILI_H
+#define MAGAZZINO_DICHIARAZIONE_VARIABILI_H
+
+
+extern int colonne;
+extern int prodotti;
+
+void seNonAbilitato();
+
+#endif //MAGAZZINO_DICHIARAZIONE_VARIABILI_H
---
+++ Funzioni_Magazzino.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Dichiarazione_Variabili.h"
+using namespace std;
+
+void seNonAbilitato(){
+    cout << "prodotti "<<prodotti;
+    cout<<"Si prega di inserire dei valori almeno una volta\n";
+}

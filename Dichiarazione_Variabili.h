1bc9841acd3adb5d65195209920d033983dff5c8
Erbonni <teobon08@gmail.com>
2026-04-29 09:24:41 +0200

    punti 4 e 6

--- Dichiarazione_Variabili.h
+++ Dichiarazione_Variabili.h
@@ -5,10 +5,7 @@
 #ifndef MAGAZZINO_DICHIARAZIONE_VARIABILI_H
 #define MAGAZZINO_DICHIARAZIONE_VARIABILI_H
 
-
-extern int colonne;
-extern int prodotti;
-//nome funzioni per il 4 e il 6 punto
+const int colonne=2;
 void piuPresente(float m[][colonne], int righe);
 void valoreMaggiore(float m[][colonne], int righe);
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "Dichiarazione_Variabili.h"
 using namespace std;
-//boh
-int colonne = 2;
-int prodotti = 7;
-
-
 int main() {
     return 0;
 }
